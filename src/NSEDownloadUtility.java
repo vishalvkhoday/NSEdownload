7941ee59129519d586928accf0d1014930ace88d
vinod <vinhabi1@in.ibm.com>
2020-11-06 20:19:46 +0530

    Added the logic to convert csv file to text file

--- src/NSEDownloadUtility.java
+++ src/NSEDownloadUtility.java
@@ -1,77 +1,109 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class NSEDownloadUtility {
-	
+
 	private static final int BUFFER_SIZE = 4096;
-	 
-    /**
-     * Downloads a file from a URL
-     * @param fileURL HTTP URL of the file to be downloaded
-     * @param saveDir path of the directory to save the file
-     * @throws IOException 
-     */
+
+	/**
+	 * Downloads a file from a URL
+	 * 
+	 * @param fileURL HTTP URL of the file to be downloaded
+	 * @param saveDir path of the directory to save the file
+	 * @throws IOException
+	 */
 	/*
 	 * initial version..
 	 * 
 	 */
-	
-    public static void downloadFile(String fileURL, String saveDir)
-            throws IOException {
-        URL url = new URL(fileURL);
-        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-        int responseCode = httpConn.getResponseCode();
- 
-        // always check HTTP response code first
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            String fileName = "";
-            String disposition = httpConn.getHeaderField("Content-Disposition");
-            String contentType = httpConn.getContentType();
-            int contentLength = httpConn.getContentLength();
- 
-            if (disposition != null) {
-                // extracts file name from header field
-                int index = disposition.indexOf("filename=");
-                if (index > 0) {
-                    fileName = disposition.substring(index + 10,
-                            disposition.length() - 1);
-                }
-            } else {
-                // extracts file name from URL
-                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
-                        fileURL.length());
-            }
- 
-            System.out.println("Content-Type = " + contentType);
-            System.out.println("Content-Disposition = " + disposition);
-            System.out.println("Content-Length = " + contentLength);
-            System.out.println("fileName = " + fileName);
- 
-            // opens input stream from the HTTP connection
-            InputStream inputStream = httpConn.getInputStream();
-            String saveFilePath = saveDir + File.separator + fileName;
-             
-            // opens an output stream to save into file
-            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
- 
-            int bytesRead = -1;
-            byte[] buffer = new byte[BUFFER_SIZE];
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
- 
-            outputStream.close();
-            inputStream.close();
- 
-            System.out.println("File downloaded");
-        } else {
-            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
-        }
-        httpConn.disconnect();
-    }
+
+	public static void downloadFile(String fileURL, String saveDir) throws IOException {
+		URL url = new URL(fileURL);
+		HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+		int responseCode = httpConn.getResponseCode();
+
+		// always check HTTP response code first
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			String fileName = "";
+			String disposition = httpConn.getHeaderField("Content-Disposition");
+			String contentType = httpConn.getContentType();
+			int contentLength = httpConn.getContentLength();
+
+			if (disposition != null) {
+				// extracts file name from header field
+				int index = disposition.indexOf("filename=");
+				if (index > 0) {
+					fileName = disposition.substring(index + 10, disposition.length() - 1);
+				}
+			} else {
+				// extracts file name from URL
+				fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1, fileURL.length());
+			}
+
+			System.out.println("Content-Type = " + contentType);
+			System.out.println("Content-Disposition = " + disposition);
+			System.out.println("Content-Length = " + contentLength);
+			System.out.println("fileName = " + fileName);
+
+			// opens input stream from the HTTP connection
+			InputStream inputStream = httpConn.getInputStream();
+			String saveFilePath = saveDir + File.separator + fileName;
+
+			// opens an output stream to save into file
+			FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+			int bytesRead = -1;
+			byte[] buffer = new byte[BUFFER_SIZE];
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
+
+			outputStream.close();
+			inputStream.close();
+
+			System.out.println("File downloaded");
+		} else {
+			System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+		}
+		httpConn.disconnect();
+	}
+
+	public static void extractTheFileFromZipFolder(String saveDir) throws IOException, FileNotFoundException {
+		byte[] buffer = new byte[2048];
+		Path outDir = Paths.get(saveDir);
+		String inputZipFileName = "C:/nse-download/cm30OCT2020bhav.csv.zip";
+
+		try (FileInputStream fis = new FileInputStream(inputZipFileName);
+				BufferedInputStream bis = new BufferedInputStream(fis);
+				ZipInputStream stream = new ZipInputStream(bis)) {
+
+			ZipEntry entry;
+			while ((entry = stream.getNextEntry()) != null) {
+
+				Path filePath = outDir.resolve(entry.getName());
+
+				try (FileOutputStream fos = new FileOutputStream(filePath.toFile());
+						BufferedOutputStream bos = new BufferedOutputStream(fos, buffer.length)) {
+
+					int len;
+					while ((len = stream.read(buffer)) > 0) {
+						bos.write(buffer, 0, len);
+					}
+				}
+			}
+		}
+	}
 
 }
--- src/NSEDownloader.java
+++ src/NSEDownloader.java
@@ -1,13 +1,10 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 /*
  * initial version
@@ -16,40 +13,76 @@ import java.util.zip.ZipInputStream;
 public class NSEDownloader {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
+
 		String fileURL = "https://www1.nseindia.com/content/historical/EQUITIES/2020/OCT/cm30OCT2020bhav.csv.zip";
-        String saveDir = "C:/test";
-        try {
-        	NSEDownloadUtility.downloadFile(fileURL, saveDir);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        
-        
-        byte[] buffer = new byte[2048];
-        Path outDir = Paths.get("C:/test/");
-        String inputZipFileName = "C:/test/cm30OCT2020bhav.csv.zip";
-        
-        try (FileInputStream fis = new FileInputStream(inputZipFileName);
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                ZipInputStream stream = new ZipInputStream(bis)) {
+		String saveDir = "C:/nse-download";
+		try {
+			NSEDownloadUtility.downloadFile(fileURL, saveDir);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 
-            ZipEntry entry;
-            while ((entry = stream.getNextEntry()) != null) {
+		NSEDownloadUtility.extractTheFileFromZipFolder(saveDir);
 
-                Path filePath = outDir.resolve(entry.getName());
+		File inputCSVFileDirPath = new File("C:/nse-download/cm30OCT2020bhav.csv");
+		File outputTxtFileDirPath = new File("C:/nse-download/cm30OCT2020bhav.txt");
+		BufferedReader brReadCSVFile = null;// For Read CSV File
+		BufferedWriter brWriteCSVDataToTextFile = null;// For Write a file in which you want to write
+		String words = "";
+		String separator = "\\,";// Here you can use , or space according to your csv file
 
-                try (FileOutputStream fos = new FileOutputStream(filePath.toFile());
-                        BufferedOutputStream bos = new BufferedOutputStream(fos, buffer.length)) {
+		try {
 
-                    int len;
-                    while ((len = stream.read(buffer)) > 0) {
-                        bos.write(buffer, 0, len);
-                    }
-                }
-            }
-        }
-		
+			brReadCSVFile = new BufferedReader(new FileReader(inputCSVFileDirPath));
+			brWriteCSVDataToTextFile = new BufferedWriter(new FileWriter(outputTxtFileDirPath));
+			while ((words = brReadCSVFile.readLine()) != null) {
+				String[] code = words.split(separator);
+				brWriteCSVDataToTextFile.write(code[0]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[1]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[2]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[3]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[4]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[5]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[6]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[7]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[8]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[9]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[10]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[11]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[12]);
+				brWriteCSVDataToTextFile.write("\n");
+				System.out.println("code1= " + code[0] + " , code2=" + code[1]);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (brReadCSVFile != null) {
+				try {
+					brReadCSVFile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			brReadCSVFile.close();
+			brWriteCSVDataToTextFile.close();
+		}
+
 	}
 
 }

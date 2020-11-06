055e14d2860fd5c3e84918f7653cf6aac03a518f
vinod <vinhabi1@in.ibm.com>
2020-11-06 20:52:53 +0530

    moved the logic to util class

--- src/NSEDownloadUtility.java
+++ src/NSEDownloadUtility.java
@@ -1,9 +1,13 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -103,6 +107,67 @@ public class NSEDownloadUtility {
 					}
 				}
 			}
+		}
+	}
+
+	public static void convertCSVFileToTextFile() throws IOException {
+		File inputCSVFileDirPath = new File("C:/nse-download/cm30OCT2020bhav.csv");
+		File outputTxtFileDirPath = new File("C:/nse-download/cm30OCT2020bhav.txt");
+		BufferedReader brReadCSVFile = null;// For Read CSV File
+		BufferedWriter brWriteCSVDataToTextFile = null;// For Write a file in which you want to write
+		String words = "";
+		String separator = "\\,";// Here you can use , or space according to your csv file
+
+		try {
+
+			brReadCSVFile = new BufferedReader(new FileReader(inputCSVFileDirPath));
+			brWriteCSVDataToTextFile = new BufferedWriter(new FileWriter(outputTxtFileDirPath));
+			while ((words = brReadCSVFile.readLine()) != null) {
+				String[] code = words.split(separator);
+				brWriteCSVDataToTextFile.write(code[0]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[1]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[2]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[3]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[4]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[5]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[6]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[7]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[8]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[9]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[10]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[11]);
+				brWriteCSVDataToTextFile.write(",");
+				brWriteCSVDataToTextFile.write(code[12]);
+				brWriteCSVDataToTextFile.write("\n");
+				System.out.println("code1= " + code[0] + " , code2=" + code[1]);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (brReadCSVFile != null) {
+				try {
+					brReadCSVFile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			brReadCSVFile.close();
+			brWriteCSVDataToTextFile.close();
 		}
 	}
 
--- src/NSEDownloader.java
+++ src/NSEDownloader.java
@@ -14,6 +14,9 @@ public class NSEDownloader {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
+		String startDate = "2020/10/30";
+		String endDate = "2020/10/30";
+
 		String fileURL = "https://www1.nseindia.com/content/historical/EQUITIES/2020/OCT/cm30OCT2020bhav.csv.zip";
 		String saveDir = "C:/nse-download";
 		try {
@@ -24,64 +27,7 @@ public class NSEDownloader {
 
 		NSEDownloadUtility.extractTheFileFromZipFolder(saveDir);
 
-		File inputCSVFileDirPath = new File("C:/nse-download/cm30OCT2020bhav.csv");
-		File outputTxtFileDirPath = new File("C:/nse-download/cm30OCT2020bhav.txt");
-		BufferedReader brReadCSVFile = null;// For Read CSV File
-		BufferedWriter brWriteCSVDataToTextFile = null;// For Write a file in which you want to write
-		String words = "";
-		String separator = "\\,";// Here you can use , or space according to your csv file
-
-		try {
-
-			brReadCSVFile = new BufferedReader(new FileReader(inputCSVFileDirPath));
-			brWriteCSVDataToTextFile = new BufferedWriter(new FileWriter(outputTxtFileDirPath));
-			while ((words = brReadCSVFile.readLine()) != null) {
-				String[] code = words.split(separator);
-				brWriteCSVDataToTextFile.write(code[0]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[1]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[2]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[3]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[4]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[5]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[6]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[7]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[8]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[9]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[10]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[11]);
-				brWriteCSVDataToTextFile.write(",");
-				brWriteCSVDataToTextFile.write(code[12]);
-				brWriteCSVDataToTextFile.write("\n");
-				System.out.println("code1= " + code[0] + " , code2=" + code[1]);
-
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (brReadCSVFile != null) {
-				try {
-					brReadCSVFile.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			brReadCSVFile.close();
-			brWriteCSVDataToTextFile.close();
-		}
+		NSEDownloadUtility.convertCSVFileToTextFile();
 
 	}
 
